b3adcd5ed86e2823a8782dff40c5f678b14dbe5d
camilayatabe <camila.yatabe@gmail.com>
2025-02-27 19:46:08 -0300

    feat: react icons

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,7 +1,15 @@
+import {FaFacebook, FaInstagram, FaLinkedin} from 'react-icons/fa'
+import styles from './Footer.module.css'
+
 function Footer(){
     return(
         <footer>
-            roda pé exemplo
+            <ul className={styles.social_list}>
+                <li> <FaFacebook/> </li>
+                <li> <FaInstagram/> </li>
+                <li> <FaLinkedin/> </li>
+            </ul>
+           
         </footer>
     )
 }
